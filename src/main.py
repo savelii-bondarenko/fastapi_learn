91a4e8be739ba7827ea0d17c382be689b72e9384
sawa <savabondarenko2016@gmail.com>
2026-03-13 19:53:55 +0200

    Secont

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Path
 from sqlalchemy.orm import Session
 
 import models
@@ -23,4 +23,10 @@ db_dependency = Annotated[Session, Depends(get_db)]
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
 
-
+@app.get("/todos/{todo_id}")
+async def get_todo_id(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo is not None:
+        return todo 
+    return "Todo is not found"
+    
